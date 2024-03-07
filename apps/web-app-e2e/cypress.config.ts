5b3593b586b11e9acec76dd9bb3bc56e77cc0f5b
Maxime Robin <maxime.a.robin@gmail.com>
2024-03-07 16:02:12 +0100

    Initial commit

---
+++ apps/web-app-e2e/cypress.config.ts
@@ -0,0 +1,14 @@
+import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
+
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    ...nxE2EPreset(__filename, {
+      cypressDir: 'src',
+      webServerCommands: { default: 'nx run web-app:start' },
+      ciWebServerCommand: 'nx run web-app:serve-static',
+    }),
+    baseUrl: 'http://localhost:3000',
+  },
+});
